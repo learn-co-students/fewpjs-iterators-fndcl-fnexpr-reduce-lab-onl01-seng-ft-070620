600aa7a0b6bdf0620a8665926288e84ddf67b340
jchasse <jonathan.r.chasse@gmail.com>
2020-10-20 12:40:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// pry = require('pryjs')
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce( function(total, element){ return element + total}, 0)
+// const arrowTotalBatteries = batteryBatches.reduce( myfunction = (total, element) => {return element + total}, 0)
+
+
+// console.log(totalBatteries)
+// console.log(arrowTotalBatteries)
